6c925c28596eb7c81d2b95b3cc2b5ba9c13eea66
shantanu143 <shantanunirapal@gmail.com>
2023-09-09 08:51:02 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,34 @@
+
+var config = {
+    
+    texts: [
+        "Start Kare 😁😉",      
+        "Bhakti, I want you to know that you hold a unique place in my heart—one that's irreplaceable. ",  
+        "Our journey together has been nothing short of extraordinary, ",
+        "filled with countless memories that I hold dear.",
+        "From our spontaneous adventures to the quiet moments of shared laughter,",
+        "each memory is a testament to the remarkable bond we share.",
+        "bus na ab or kitna tarif sunegi bhendi",
+        "Having you as my best friend is like",
+        "Having an emotionally unstable daughter that never listens.😒😒😒😒",
+        "Suna kar na bhendi 😶‍🌫️😤",
+        "Smaji ????????????",
+        "Happy Brithday buddy 🙌🥳😙",
+    ],
+
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+      
+    },
+    
+    desc: {
+        turn_on: "Start Kare 😁😉",
+        play: "Song To Banata Hai 🎶🎧🎹",
+        bannar_coming: "Are Bhakti CLick To Kar 😒",
+        balloons_flying: "Balloons Chahiye Na 🎈😁😊",
+        cake_fadein: "Abhi Cake bhi to chahiye hoga na 🎂🍰🙌",
+        light_candle: "Candle Jalane kon Ayega 😒😒",
+        wish_message: "Happy Brithday yahhhhhh 🥳🙌🥂🍾",
+        story: "Hamesha Rost Karta Hoon Na isbar achi bat 😁 ",
+    }
+};
